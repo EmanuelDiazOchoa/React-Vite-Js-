9d139b666db0282673584ec34c01599b54db845e
emanuel diaz ochoa <emagnr93@gmail.com>
2024-10-18 22:15:08 -0300

    guardando

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <nav className="bg-blue-500 p-4">
       <div className="container mx-auto flex justify-between items-center">
-        <a className="text-white text-xl font-bold" href="#">Nombre de la Tienda</a>
+        <a className="text-white text-xl font-bold" href="#">Salem</a>
         <div className="flex space-x-4">
           <a className="text-white hover:underline" href="#">Categoría 1</a>
           <a className="text-white hover:underline" href="#">Categoría 2</a>
