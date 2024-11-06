316888b1f78b8d23ee62672a13a71c6e4ffb7b30
emanuel diaz ochoa <emagnr93@gmail.com>
2024-11-05 22:06:39 -0300

    probando tailwind

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -7,7 +7,7 @@ const CartWidget = () => {
     <div className="relative">
       <FontAwesomeIcon icon={faShoppingCart} size="2x" className="text-white" />
       <span className="absolute top-0 right-0 transform translate-x-1/2 -translate-y-1/2 bg-red-600 text-white text-xs font-bold rounded-full w-5 h-5 flex items-center justify-center">
-        5
+        0
       </span>
     </div>
   );
