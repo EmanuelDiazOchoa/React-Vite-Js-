5ef348e05808ee07ba7007c0235c472176112022
emanuel diaz ochoa <emagnr93@gmail.com>
2024-11-12 20:41:26 -0300

    pruebas

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,25 +1,34 @@
 import React from 'react';
 import Carrusel from './Carrusel'; 
 import Categorias from './Categorias'; 
-import ItemListContainer from './ItemListContainer'; 
+import ItemListContainer from './ItemListContainer';
+import ProductGallery1 from './ProductGallery1';
 
 const Home = () => {
 return (
     <div>
-      
+
+    
     <Carrusel />
 
-      
+    
     <div className="categorias-container my-16">
         <h2 className="text-center text-2xl font-bold">Explora nuestras Categorías</h2>
         <Categorias />
     </div>
 
-      
+    
     <div className="productos-container my-16">
         <h2 className="text-center text-2xl font-bold">Productos Destacados</h2>
         <ItemListContainer />
     </div>
+
+    
+    <div className="product-gallery my-16">
+        <h2 className="text-center text-2xl font-bold">Productos Recomendados</h2>
+        <ProductGallery1 />
+    </div>
+
     </div>
 );
 };
