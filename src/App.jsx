79f88282af15afe260970d3826441f629438e46c
emanuel diaz ochoa <emagnr93@gmail.com>
2024-11-28 21:59:03 -0300

    react

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import Navbar from './components/Navbar';
+import Navbar from './components/Navbarcomponent';
 import ItemListContainer from './components/ItemListContainer';
 import ItemDetailContainer from './components/ItemDetailContainer';
 import { CartProvider } from './context/CartContext';
 import { CheckoutProvider } from './context/CheckoutContext';
-// import Home from './components/Home';
+import Home from './components/Homecomponent';
 import Cart from './components/Cart';
-// import Footer from './components/Footer.jsx'; 
+import Footer from './components/Footercomponent'; 
 import Checkout from './components/checkout';
 
 
@@ -17,17 +17,17 @@ function App() {
       <CheckoutProvider>
         <BrowserRouter>
           <div className="flex flex-col min-h-screen">
-            {/* <Navbar /> */}
+            <Navbar />
             <main className="flex-grow pt-[84px]">
               <Routes>
-                {/* <Route path="/" element={<Home />} /> */}
+                <Route path="/" element={<Home />} />
                 <Route path="/categoria/:categoryId" element={<ItemListContainer />} />
                 <Route path="/item/:id" element={<ItemDetailContainer />} />
                 <Route path="/cart" element={<Cart />} />
                 <Route path="/checkout" element={<Checkout />} />
               </Routes>
             </main>
-            {/* <Footer /> */}
+            <Footer />
           </div>
         </BrowserRouter>
       </CheckoutProvider>
